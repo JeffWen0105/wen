3819ba4e67d606a1617ed441e4962e4793d0ee18
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2020-03-05 22:24:42 +0800

    update

---
+++ iiiEduBdse/release/hadoop_line_notify/bdse_linenotify.py
@@ -0,0 +1,39 @@
+import sys
+import requests
+import datetime
+
+
+
+def get_conf():
+    f = open('bdse-conf.txt', 'r')
+    token = f.read()
+    token = token.strip('\n')
+    f.close()
+    return token
+
+def time_set():
+    ISOTIMEFORMAT = '%Y/%m/%d %H:%M'
+    the_time = datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    return the_time
+    
+def line_notify(state,token,message):
+        requests.post(
+            url='https://notify-api.line.me/api/notify',
+            headers={
+                'Authorization': f'Bearer {token}'
+            },
+            data={
+                'message': "{} {}, 發生時間:{}".format(message,state,the_time)
+            }
+        )
+
+
+if __name__ == '__main__':
+    status = sys.argv[1]
+    message = sys.argv[2]
+    token = get_conf()
+    the_time = time_set()
+    if status == 'good' :
+        line_notify("恢復",token,message)
+    else :
+        line_notify("停止",token,message)
