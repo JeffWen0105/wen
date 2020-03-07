3365370c77613adc8dee16ee442bc83280acf143
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2020-03-07 23:00:12 +0800

    Update hyper.js

--- iiiEduBdse/release/hyper/hyper.js
+++ iiiEduBdse/release/hyper/hyper.js
@@ -102,7 +102,7 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-	"hyper-alt-click","hyperpower"
+	"hyper-alt-click","hyperpower","hyper-native"
 	],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
